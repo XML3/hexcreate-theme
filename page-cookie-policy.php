91fc46d7723cd101775656e0f1435db6eaae9f52
Xagly Montilva <xaglymontilva@gmail.com>
2026-06-17 19:06:39 +0200

    new file cookie policy

---
+++ page-cookie-policy.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Template Name: Cookie Policy
+*/
+get_header();
+?>
+<main>
+    <div class="container-wide py-12 bg-primary">
+        <div class="container-content font-inter normal-16 text-title">
+    <?php echo do_shortcode('[faz_cookie_policy_complete]'); ?>
+    </div>
+    </div>
+</main>
+<?php get_footer(); ?>
